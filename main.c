8a4112e5f7d8dfe2fd9b2e97718fae8460c57029
Ken <kenhiowatha@gmail.com>
2019-03-01 17:17:37 +0800

    (Redo by P46071262) Using NULL in array of func ptr.

--- main.c
+++ main.c
@@ -7,17 +7,13 @@ int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
 int main()
 {
+	
 	int a, c;
 	char b;
-	printf("key a Function \nEX: 1 + 1\n");
+	int (*operator[6])(int,int)={multiply,plus,NULL,minus,NULL,divided};
+	printf("(Redo By P46071262)key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	//REDO
+	printf("%d %c %d = %d\n", a, b, c, operator[b-42](a, c));
+
 }
